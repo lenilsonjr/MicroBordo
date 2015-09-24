2938e5cb34eafc2672b4403bae3c49787ce40141
Lenilson Junior <lenilsonjunior.jr@gmail.com>
2015-09-24 15:36:44 -0300

    Added SQL file.

---
+++ sql/microbordo.sql
@@ -0,0 +1,80 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.7
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Tempo de geração: 24/09/2015 às 18:36
+-- Versão do servidor: 10.0.19-MariaDB
+-- Versão do PHP: 5.6.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Banco de dados: `microbordo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `options`
+--
+
+CREATE TABLE IF NOT EXISTS `options` (
+  `id` int(10) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `value` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `posts`
+--
+
+CREATE TABLE IF NOT EXISTS `posts` (
+  `id` int(10) NOT NULL,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `post` mediumtext NOT NULL,
+  `tags` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Índices de tabelas apagadas
+--
+
+--
+-- Índices de tabela `options`
+--
+ALTER TABLE `options`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas apagadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `options`
+--
+ALTER TABLE `options`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de tabela `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
